35ce995f0da2cb046c2d15de642d11b06097d83a
Milos Popovic <milos_popovic@outlook.com>
2020-07-22 15:59:13 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+require 'pry'
+class Song
+  @@artists = []
+  @@genres = []
+  @@count = 0
+
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    #@@genres.uniq!
+    @@artists << artist
+    @@artists.uniq!
+
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists
+  end
+  def self.genres
+    @@genres
+  end
+
+  def self.genre_count
+      genre_count_hash = {}
+      @@genres.each do |genre|
+        if genre_count_hash[genre]
+          genre_count_hash[genre] += 1
+        else
+          genre_count_hash[genre] = 1
+        end
+      end
+      genre_count_hash
+  end
+
+  def self.artist_count
+
+  end
+
+end
